e8794638b06709ed88202ab8aeec17f035734ccf
Вероника Мазуренко <veronikavv1732@gmail.com>
2025-12-08 15:23:55 +0300

    add tests

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.category import Category
 from src.product import Product
 
@@ -11,15 +12,21 @@ def reset_category_counters():
 
 @pytest.fixture
 def first_category():
-    return Category(name='Смартфоны',
-                description='Смартфоны, как средство не только коммуникации, '
-                            'но и получения дополнительных функций для удобства жизни',
-                products=[
-                    Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
-                          Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
-                          Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
-                          ],
-                    )
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, "
+        "но и получения дополнительных функций для удобства жизни",
+        products=[
+            Product(
+                "Samsung Galaxy S23 Ultra",
+                "256GB, Серый цвет, 200MP камера",
+                180000.0,
+                5,
+            ),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
+        ],
+    )
 
 
 @pytest.fixture()
@@ -27,7 +34,7 @@ def second_category():
     return Category(
         name="Телевизоры",
         description="Современный телевизор, который позволяет наслаждаться просмотром, "
-                    "станет вашим другом и помощником",
+        "станет вашим другом и помощником",
         products=[
             Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7),
             Product('75" QLED 4K', "Смарт", 125000.0, 6),
@@ -37,8 +44,10 @@ def second_category():
 
 def test_category_init(first_category, second_category):
     assert first_category.name == "Смартфоны"
-    assert (first_category.description == "Смартфоны, как средство не только коммуникации, "
-                                      "но и получения дополнительных функций для удобства жизни")
+    assert (
+        first_category.description == "Смартфоны, как средство не только коммуникации, "
+        "но и получения дополнительных функций для удобства жизни"
+    )
     assert len(first_category.products) == 3
 
     assert first_category.category_count == 2
--- tests/test_product.py
+++ tests/test_product.py
@@ -5,25 +5,27 @@ from src.product import Product
 
 @pytest.fixture
 def product_1():
-    return Product(name='Samsung Galaxy S23 Ultra',
-                description='256GB, Серый цвет, 200MP камера',
-                price=180000.0,
-                quantity=5)
+    return Product(
+        name="Samsung Galaxy S23 Ultra",
+        description="256GB, Серый цвет, 200MP камера",
+        price=180000.0,
+        quantity=5,
+    )
 
 
 @pytest.fixture
 def product_2():
-    return Product(name='Iphone 15',
-                description='512GB, Gray space',
-                price=210000.0,
-                quantity=8)
+    return Product(name="Iphone 15", description="512GB, Gray space", price=210000.0, quantity=8)
+
 
 @pytest.fixture
 def product_3():
-    return Product(name='Xiaomi Redmi Note 11',
-                description='1024GB, Синий',
-                price=31000.0,
-                quantity=14)
+    return Product(
+        name="Xiaomi Redmi Note 11",
+        description="1024GB, Синий",
+        price=31000.0,
+        quantity=14,
+    )
 
 
 def test_product_1(product_1):
